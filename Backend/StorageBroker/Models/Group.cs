f9550740c2c6a8676133ab0c585de8390d6ec7d5
Abdumannon Shamsyev <0605AbMu@gmail.com>
2022-12-17 17:39:27 +0500

    changed StorageBroker.Model.Group and created Enum of StorageBroker.Model.GroupStatus

--- Backend/StorageBroker/Models/Group.cs
+++ Backend/StorageBroker/Models/Group.cs
@@ -2,5 +2,14 @@
 
 public class Group
 {
-    
+    public int GroupId { get; set; }
+    public string Name { get; set; }
+    public GroupStatus Status { get; set; }
+    public int CountOfStudents { get; set; }
+}
+
+public enum GroupStatus
+{
+    Finished,
+    Continued
 }
