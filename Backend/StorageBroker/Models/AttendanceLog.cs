45f3889fde453ac49bdb770abf3019d56577a763
Elchin Uralov <elchinuralov07@gmail.com>
2022-12-17 17:41:15 +0500

    Created AttendanceLog class in Model

---
+++ Backend/StorageBroker/Models/AttendanceLog.cs
@@ -0,0 +1,18 @@
+﻿
+namespace StorageBroker.Models
+{
+    public class AttendanceLog
+    {
+        public int AttendanceLogId { get; set; }
+        public int GroupId { get; set; }
+        public int StudentId { get; set; }
+        public int Mark { get; set; }
+        public TypeOfGrade Grade { get; set; }
+    }
+    public enum TypeOfGrade
+    {
+        DailyGrade,
+        HomeworkGrade,
+        ExamGrade
+    }
+}
