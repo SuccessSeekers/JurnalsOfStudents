cfa9e23770e993dd082e437020080728c340c0ee
Elchin Uralov <elchinuralov07@gmail.com>
2022-12-17 18:04:02 +0500

    Edited AttendanceLog Class  in Models

--- Backend/StorageBroker/Models/AttendanceLog.cs
+++ Backend/StorageBroker/Models/AttendanceLog.cs
@@ -4,7 +4,14 @@
     {
         public int AttendanceLogId { get; set; }
         public int GroupId { get; set; }
-        public int StudentId { get; set; }
-        public int Mark { get; set; }
+        public DateTime Date { get; set; }
+        public int LateTime { get; set; }
+        public LateType Status { get; set; }
+    }
+    public enum LateType
+    {
+        DidNotCome,
+        Came,
+        CameLate
     }
 }
