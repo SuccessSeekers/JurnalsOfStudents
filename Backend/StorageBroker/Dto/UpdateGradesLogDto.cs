2c3e59e70f84e8cd66fd7d4af3bc4b6e02169c26
Abdumannon Shamsyev <0605AbMu@gmail.com>
2022-12-18 16:40:08 +0500

    Bugs fixed on GradesLogController.cs

---
+++ Backend/StorageBroker/Dto/UpdateGradesLogDto.cs
@@ -0,0 +1,10 @@
+﻿using StorageBroker.Models;
+
+namespace StorageBroker.Dto;
+
+public class UpdateGradesLogDto
+{
+    public TypeOfGrade Grade { get; set; }
+    public int StudentId { get; set; }
+    public int GroupId { get; set; }
+}
