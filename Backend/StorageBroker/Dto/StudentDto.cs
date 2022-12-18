80d832fe4b3bee1c2b0f0e9b9fc5abe434aeef17
Elchin Uralov <elchinuralov07@gmail.com>
2022-12-18 19:26:59 +0500

    Finally Updated Student controller

---
+++ Backend/StorageBroker/Dto/StudentDto.cs
@@ -0,0 +1,12 @@
+﻿using StorageBroker.Models;
+
+namespace StorageBroker.Dto;
+
+public class StudentDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public ICollection<GradesLog> Grades { get; set; }
+    public ICollection<AttendanceLog> Attendances { get; set; }
+    public ICollection<Group> StudentGroups { get; set; }
+}
--- Backend/WebAPI/Controllers/StudentController.cs
+++ Backend/WebAPI/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using StorageBroker.Dto;
 using StorageBroker.Models;
 using StorageBroker.RepositoryManager;
+using System.Security.Cryptography;
 using WebAPI.Dto;
 
 namespace WebAPI.Controllers;
@@ -19,63 +20,100 @@ public class StudentController : ControllerBase
 
     [HttpGet]
     [Produces(typeof(ResponseDto<Student>))]
-    public IActionResult GetStudents()
+    public ResponseDto<Student> GetStudents()
     {
-        return Ok(repositoryManager.StudentRepository.GetAll());
+        return new ResponseDto<Student>(repositoryManager.StudentRepository.GetAll());
     }
 
     [HttpGet("{StudentId:int}")]
     [Produces(typeof(ResponseDto<Student>))]
-    public IActionResult GetStudentById(int id)
+    public ResponseDto<Student> GetStudentById(int id)
     {
         var student = repositoryManager.StudentRepository
             .GetAllWithCondition(student => student.StudentId == id);
 
         if (student is null)
-            throw new Exception("Student not found");
+            return new ResponseDto<Student>(StatusCodes.Status400BadRequest, new Exception("Student not found"));
 
-        return Ok(student);
+        return new ResponseDto<Student>(student);
     }
 
     [HttpPost]
-    [Produces(typeof(ResponseDto<Student>))]
-    public async Task CreateStudent([FromBody] Student student)
+    [Produces(typeof(ResponseDto<StudentDto>))]
+    public async Task<ResponseDto<StudentDto>> CreateStudent([FromBody] CreateStudentDto createStudentDto)
     {
         var newStudent = new Student();
-        newStudent.Name = student.Name;
-        newStudent.Surname = student.Surname;
+        newStudent.Name = createStudentDto.Name;
+        newStudent.Surname = createStudentDto.Surname;
 
+        var createdStudent = await repositoryManager.StudentRepository
+            .CreateAsync(newStudent);
 
-        await repositoryManager.StudentRepository.CreateAsync(newStudent);
+        await repositoryManager.SaveAsync();
 
-        repositoryManager.Save();
+        var studentDto = new StudentDto()
+        {
+            Name = createdStudent.Name,
+            Surname = createdStudent.Surname,
+            Grades = createdStudent.Grades,
+            Attendances = createdStudent.Attendances,
+            StudentGroups = createdStudent.StudentGroups
+        };
+
+        return new ResponseDto<StudentDto>(studentDto);
     }
 
     [HttpPut]
-    [Produces(typeof(ResponseDto<Student>))]
-    public void UpdateStudent([FromBody] Student student)
-    {
-        var newStudent = new Student();
-        newStudent.Name = student.Name;
-        newStudent.Surname = student.Surname;
-
-        repositoryManager.StudentRepository.Update(newStudent);
-
-        repositoryManager.Save();
-    }
-
-    [HttpDelete]
-    [Produces(typeof(int))]
-    public void DeleteStudentById(int id)
+    [Produces(typeof(ResponseDto<StudentDto>))]
+    public async Task<ResponseDto<StudentDto>> UpdateStudentById(int id, [FromBody] UpdateStudentDto updateStudent)
     {
         var oldStudent = repositoryManager.StudentRepository.GetAll()
             .FirstOrDefault(student => student.StudentId == id);
 
         if (oldStudent is null)
-            throw new Exception("Student not found");
+            return new ResponseDto<StudentDto>(404, new Exception("Student not found"));
+
+        oldStudent.Name = updateStudent.Name;
+        oldStudent.Surname = updateStudent.Surname;
+
+        repositoryManager.StudentRepository.Update(oldStudent);
+
+        await repositoryManager.SaveAsync();
+
+        var studentDto = new StudentDto()
+        {
+            Name = oldStudent.Name,
+            Surname = oldStudent.Surname,
+            Grades = oldStudent.Grades,
+            Attendances = oldStudent.Attendances,
+            StudentGroups = oldStudent.StudentGroups
+        };
+
+        return new ResponseDto<StudentDto>(studentDto);
+    }
+
+    [HttpDelete]
+    [Produces(typeof(ResponseDto<StudentDto>))]
+    public async Task<ResponseDto<StudentDto>> DeleteStudentById(int id)
+    {
+        var oldStudent = repositoryManager.StudentRepository.GetAll()
+            .FirstOrDefault(student => student.StudentId == id);
+
+        if (oldStudent is null)
+            return new ResponseDto<StudentDto>(StatusCodes.Status400BadRequest, new Exception("Group not found"));
 
         repositoryManager.StudentRepository.Delete(oldStudent);
 
-        repositoryManager.Save();
+        await repositoryManager.SaveAsync();
+
+        var studentDto = new StudentDto()
+        {
+            Name = oldStudent.Name,
+            Surname = oldStudent.Surname,
+            Grades = oldStudent.Grades,
+            Attendances = oldStudent.Attendances,
+            StudentGroups = oldStudent.StudentGroups
+        };
+        return new ResponseDto<StudentDto>(studentDto);
     }
 }
